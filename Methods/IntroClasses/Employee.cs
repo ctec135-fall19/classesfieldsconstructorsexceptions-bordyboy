6e2f352ba1a09ee029fce54dab2f10bb4e79246c
Bogdan Livadaru <bogdanlivadaru@yahoo.com>
2019-10-10 15:17:20 -0700

    Yes

---
+++ Methods/IntroClasses/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroClasses
+{
+    public class Employee
+    {
+        public void Hello()
+        {
+            Console.WriteLine("hello world!");
+        }
+    }
+}
